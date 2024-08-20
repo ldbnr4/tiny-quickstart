b508fdb409381c6e84999abce5d789108098f327
Lorenzo Boyice <renzob.kc@gmail.com>
2024-08-20 04:39:55 -0700

    trying jest config

---
+++ vanilla_typescript/jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  roots: ['<rootDir>'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverage: true,
+  clearMocks: true,
+  coverageDirectory: "coverage",
+};
