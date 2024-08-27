0d17ea4200c2be3eca79227a59cf9c87f2d734e0
Lorenzo Boyice <renzob.kc@gmail.com>
2024-08-27 09:15:21 -0700

    remove plaid env test

--- vanilla_typescript/test/plaid.test.ts
+++
@@ -1,9 +0,0 @@
-import { config } from '../src/plaid'
-
-describe("Test plaid code", () => {
-    test("Env set", () => {
-        expect(config.basePath?.length).toBeGreaterThan(0)
-        expect(config.baseOptions?.headers["PLAID-CLIENT-ID"].length).toBeGreaterThan(0)
-        expect(config.baseOptions?.headers["PLAID-SECRET"].length).toBeGreaterThan(0)
-    });
-})
